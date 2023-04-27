cf0dafa790b7daabc8592733bd4a28f32f53cf73
Faiem-qy <f.quanyoung@gmail.com>
2023-04-27 17:49:01 -0400

    updated express_server to generate a shortURL and save generated ID to the longURL

--- express_server.js
+++ express_server.js
@@ -11,13 +11,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -26,7 +29,37 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = { id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+  res.redirect("/urls/:id");
+});
+
+
+function generateRandomString() {
+  const alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += alphanumeric.charAt(Math.floor(Math.random() * alphanumeric.length));
+  }
+  return result;
+}
