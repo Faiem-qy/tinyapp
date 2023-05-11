d265d275eebef968fb95927a756e190bdae94d60
Faiem-qy <f.quanyoung@gmail.com>
2023-05-10 20:06:20 -0400

    remove commented code and console logs in server

--- express_server.js
+++ express_server.js
@@ -12,12 +12,6 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
-
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -32,7 +26,6 @@ const urlDatabase = {
     userID: "user2ID",
   },
 };
-console.log(urlDatabase);
 
 const users = {
   aJ48lW: {
@@ -46,7 +39,6 @@ const users = {
     password: bcrypt.hashSync("pass", 10),
   },
 };
-console.log(users);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -73,7 +65,7 @@ app.get("/urls/new", (req, res) => {
     user_id
   };
 
-  if (!user_id) {// redirecrt to get login if the cookie doesn't exist
+  if (!user_id) {
     res.redirect('/login');
   } else {
     res.render("urls_new", templateVars);
@@ -81,7 +73,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;//grab id from address bar
+  const id = req.params.id;
   const user_id = req.session.user_id;
   if (!user_id) {
     res.send("<html><head><title>Error</title></head><body><h1>🛑🛑🛑You need to be logged in!! 🛑🛑🛑</h1></body></html>");
@@ -102,16 +94,12 @@ app.get("/urls/:id", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const user_id = req.session.user_id;
-  console.log("get /urls", user_id);
-  // console.log(users);
-  // console.log(urlDatabase);
-  console.log("get /urls session", req.session);
   const templateVars = {
     urls: urlsForUser(user_id, urlDatabase),
     users,
     user_id
   };
-  if (!user_id) {// display error if user is not logged in
+  if (!user_id) {
     res.send("<html><head><title>Error</title></head><body><h1>🤚🤚You need to be logged in to access My URL's 🚫🚏</h1></body></html>");
   } else {
 
@@ -133,7 +121,7 @@ app.get("/register", (req, res) => {
   const user_id = req.session.user_id;//check if user is logged in by checking for cookie
   // if already registered then go to /urls
   if (user_id) {
-    const id = req.params.id;//grab id from address bar
+    const id = req.params.id;
     const templateVars = {
       id,
       longURL: urlDatabase[id].longURL,
@@ -149,11 +137,9 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  const user_id = req.session.user_id;// check if user is logged in by verifying cookie
-  // if already registered then go to /urls
-  console.log('get/login user id', user_id);
+  const user_id = req.session.user_id;
   if (user_id) {
-    const id = req.params.id;//grab id from address bar
+    const id = req.params.id;
     const templateVars = {
       id,
       longURL: urlDatabase[id].longURL,
@@ -184,13 +170,10 @@ app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const id = generateRandomString();
   const user_id = req.session.user_id;
-  console.log(urlDatabase);
-  console.log(req.body);
   urlDatabase[id] = { "longURL": longURL, "userID": user_id };// assign new key value pair into urlDatabase object
   if (!user_id) {
     return res.send("You need to be logged in to perform this action!!😎 ");
   }
-  console.log(urlDatabase);
 
   res.redirect(`/urls/${id}`);
 });
@@ -227,7 +210,6 @@ app.post("/urls/:id/delete", (req, res) => {
 
 
 app.post("/logout", (req, res) => {
-  // res.clearCookie('user_id');
   req.session = null;
   res.redirect("/login");
 });
@@ -242,11 +224,7 @@ app.post("/register", (req, res) => {
     return res.status(400).send("Error 400 - Email already exists");
   } else {
     users[id] = { id, email, password };
-    // res.cookie('user_id', users[id].id);
     req.session.user_id = users[id].id;
-    console.log(users);
-    console.log("/register - session", req.session.user_id);
-
   }
   res.redirect(`/urls`);
 });
@@ -254,9 +232,7 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  console.log(email, password);
   const user_id = getUserId(email, users);
-  console.log("user_id", user_id);
   if (email === '' || password === '') {
     return res.status(400).send("Error 400 - Please provide valid email and/or password");
   }
@@ -265,7 +241,6 @@ app.post("/login", (req, res) => {
   }
   if (getUserByEmail(email, users)) {
     if (checkUserPassword(password, users)) {
-      // res.cookie('user_id', users[user_id].id);
       req.session.user_id = users[user_id].id;
     } else {
       return res.status(403).send("Error 403 - Incorrect Password!!");
